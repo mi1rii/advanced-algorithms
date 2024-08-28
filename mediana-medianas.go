ae375b09db8352eb1eb2a3e2afb57f219807d04d
mii1rii <mirandaarronizz@gmail.com>
2024-08-27 23:26:47 -0600

    Done with finding the medians of each sublist

--- mediana-medianas.go
+++ mediana-medianas.go
@@ -16,8 +16,25 @@ func particionar(lista []int) [][]int{
 	return sublistas
 }
 
+func buscarMediana(lst []int) int {
+	tamañoLista:= len(lst)
+	for j:=0; j < tamañoLista; j++ {
+		for k:= j + 1; k < tamañoLista; k++ {
+			if lst[j] > lst[k] {
+				lst[j], lst[k] = lst[k], lst[j]
+			}
+		}
+	}
+	return lst[tamañoLista / 2]
+}
+
 func main() {
 	lista:= []int{3, 8, 2, 1, 7, 9, 12, 11, 10, 5, 6, 4}
 	sublistas:= particionar(lista)
 	fmt.Println(sublistas)
+
+	for _, sublista:= range sublistas {
+		mediana:= buscarMediana(sublista)
+		fmt.Println(mediana)
+	}
 }
